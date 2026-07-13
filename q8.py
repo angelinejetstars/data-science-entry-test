01d17def824b37681387ebf37fc59361e0c6142c
angelinejetstars <angelinejetstars@gmail.com>
2026-07-14 00:23:17 +0800

    Prompt rewrite
    Added a detailed analysis of AI prompt effectiveness and provided a rewritten prompt for a data analyst's presentation to the CEO.

---
+++ q8.py
@@ -0,0 +1,49 @@
+Q8a: Which prompt will get a better response from an AI? 
+Prompt B 
+
+Q8b: Give two reasons
+First reason is Prompt B sets the context more clearly by mentioning 
+(a) the task 
+(b) the dataset 
+(c) step-by-step instructions on how the AI is to analyse 
+(d) audience 
+(e) constraints 
+
+Second reason is Prompt B gave the AI a persona, 
+explaining that he is a data analyst helping a retail marketing team
+(instead of mentioning the role; ie a marketing manager at a retail company. 
+ 
+Q8c: One strength of prompt A 
+Prompt A states the specific role (marketing manager at a retail company) 
+and timeline ("just" finished three month campaign, and that meeting is next Friday) 
+which AI can use to tailor the response. 
+
+
+Task 2: Rewrite prompt 
+Role: You are a data analyst helping a marketing team at a retail company. 
+
+Task: Prepare a presentation to the CEO from the results of a three-month 
+marketing campaign that has just finished. The campaign collected customer feedback through an online survey. 
+
+Data: 500 responses stored in a spreadsheet containing: 
+(a) customer's age group'
+(b) product purchased
+(c) satisfaction rating (1-5)
+(d) short written comment 
+
+Steps: 
+1. Identify age groups and products with highest and lowest satisfaction ratings
+2. Extract the most common themes from the written comments. 
+3. Summarize the findings into one or two paragraphs, highlighting key insights and trends.
+4. Provide recommendations for future marketing campaigns based on the analysis.
+
+Constraints: Keep summary concise, actionable, and free of technical jargon. 
+
+Explanation of what I borrowed: 
+I took Prompt A's contextual element (present findings to CEO next Friday) and added 
+to the Task in Prompt B. This provides context for urgency and high stakes 
+for the executive summary. I also provided the context that the dataset was collected 
+through online customer survey. 
+
+I also added a step to provide recommendations for future marketing campaigns 
+based on the analysis as that is a potential question from the CEO. 
